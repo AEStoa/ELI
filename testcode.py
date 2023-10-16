2b43f8e116f50c49408df03c7cfa5c46983bed8c
AEStoa <anthony.stoa@gmail.com>
2023-10-15 20:30:46 -0400

    Catagory, Value

--- testcode.py
+++ testcode.py
@@ -1,5 +1,18 @@
+import sys
+sys.stdout.flush()
 import pandas as pd
 file_path = 'FakeingredientC02e.csv'
-df = pd.read_csv(file_path)
-print(df)
+df = pd.read_csv(file_path) #reads database file
+
+user_input = input("Enter a category: ") #user input (recipe)
+
+result = df[df['Category'] == user_input]['Value'].values
+
+if len(result) > 0:
+    print(f"The corresponding value for category '{user_input}' is: {result[0]}")
+else:
+    print(f"No matching category found for '{user_input}'")
+
+
+#print (df)
 
