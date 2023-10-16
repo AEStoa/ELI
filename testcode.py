008015c05884d6fc4fb787a3ad03d2d7adcaecc0
AEStoa <anthony.stoa@gmail.com>
2023-10-15 20:33:53 -0400

    Multiplier

--- testcode.py
+++ testcode.py
@@ -4,15 +4,17 @@ import pandas as pd
 file_path = 'FakeingredientC02e.csv'
 df = pd.read_csv(file_path) #reads database file
 
-user_input = input("Enter a category: ") #user input (recipe)
+user_category = input("Enter a category: ") #user input (recipe)
+user_number = float(input("Enter a number: "))  # Convert input to float for numeric operations
 
-result = df[df['Category'] == user_input]['Value'].values
+# Find the corresponding value for the user category from the database
+filtered_df = df[df['Category'] == user_category]
 
-if len(result) > 0:
-    print(f"The corresponding value for category '{user_input}' is: {result[0]}")
+if not filtered_df.empty:
+    database_value = filtered_df['Value'].iloc[0]  # Get the first matching value from the DataFrame
+    result = database_value * user_number
+    print(f"The calculated result for category '{user_category}' multiplied by {user_number} is: {result}")
 else:
-    print(f"No matching category found for '{user_input}'")
+    print(f"No matching category found for '{user_category}'")
 
-
-#print (df)
 
